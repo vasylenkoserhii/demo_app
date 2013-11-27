36dcec964113c2aab604f84877749f2dbf5b7808
snv <snv@debian.localdomain>
2013-11-28 00:05:23 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DempApp::Application.config.secret_token = 'cbd6fa82f794df449b2207d8f84640068a7ddec8ffd08d9de75ee4baf729711d21555bd244822b91544071a568ff6ab04d99f2855128dd8b112f09e2db9f43eb'
